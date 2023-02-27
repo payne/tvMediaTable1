d079399be233d518f6d8183a00016e282afbd65b
Matt Payne <Payne@MattPayne.org>
2023-02-27 17:12:47 -0600

    ng generate component tv-grid1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TvGrid1Component } from './tv-grid1/tv-grid1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TvGrid1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tv-grid1/tv-grid1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-grid1',
+  templateUrl: './tv-grid1.component.html',
+  styleUrls: ['./tv-grid1.component.scss']
+})
+export class TvGrid1Component {
+
+}
