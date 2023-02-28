ad5beb7efd0b795dd5f1717890f43aafacb20873
Matt Payne <Payne@MattPayne.org>
2023-02-28 17:35:38 -0600

    ng generate component api-key

---
+++ src/app/api-key/api-key.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-key',
+  templateUrl: './api-key.component.html',
+  styleUrls: ['./api-key.component.scss']
+})
+export class ApiKeyComponent {
+
+}
