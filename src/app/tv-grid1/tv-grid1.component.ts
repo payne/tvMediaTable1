28ea879e9b19836405187f0611a1d4454ffe42a3
Matt Payne <Payne@MattPayne.org>
2023-02-27 21:31:40 -0600

    Simple list of channels and shows

--- src/app/tv-grid1/tv-grid1.component.ts
+++ src/app/tv-grid1/tv-grid1.component.ts
@@ -6,17 +6,30 @@ import {TvListingsService} from "../tv-listings.service";
   templateUrl: './tv-grid1.component.html',
   styleUrls: ['./tv-grid1.component.scss']
 })
-export class TvGrid1Component implements  OnInit {
+export class TvGrid1Component implements OnInit {
+  tvData: any;
+  channels: any;
 
+  constructor(private tvListings: TvListingsService) {
+  }
 
- constructor(private tvListings: TvListingsService) {
- }
-
-ngOnInit(): void {
-  this.tvListings.getListingsGrid("junk").subscribe((data) => {
-    console.log(data);
-  });
-}
+  ngOnInit(): void {
+    this.tvListings.getListingsGrid("junk").subscribe((data) => {
+      console.log(data);
+      this.tvData = data;
+      this.channels = [];
+      for (const n in data) {
+        // @ts-ignore
+        const d = data[n];
+        const pair = {
+          'channel': d['channel'],
+          'listings': d['listings']
+        };
+        // @ts-ignore
+        this.channels.push(pair);
+      }
+    });
+  }
 
 
 }
